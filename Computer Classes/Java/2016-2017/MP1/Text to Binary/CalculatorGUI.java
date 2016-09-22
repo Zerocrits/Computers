0f310faa1aebdc6202f49bfe321d221844853ec4
Zerocrits <12242864@wayneschools.net>
2016-09-22 08:06:42 -0400

    fgh

--- Computer Classes/Java/2016-2017/MP1/Text to Binary/CalculatorGUI.java
+++ Computer Classes/Java/2016-2017/MP1/Text to Binary/CalculatorGUI.java
@@ -4,33 +4,79 @@ import javax.swing.*;
 
 public class CalculatorGUI extends JFrame
 {
-	private int APPLET_WIDTH = 300, APPLET_HEIGHT = 220;
-	private JLabel lblString;
+	private int APPLET_WIDTH = 900, APPLET_HEIGHT = 800;
+	private JLabel lblBinary, lblOctal, lblDecimal, lblHex, lblTitle, lblBox1;
 	private JButton btnEnter;
-	private JTextField txtInsert;
+	private JTextField txtInsert, txtBinary, txtOctal, txtDecimal, txtHex;
+	private JRadioButton rdoBinary1, rdoBinary2;
 	private BinaryConverter binary;
+	private ButtonGroup grpButtons1;
 	private ButtonListener listener;
 
 	public CalculatorGUI()
 	{
 		Container cp = getContentPane();
-		cp.setBackground(Color.red);
 		cp.setLayout(new FlowLayout());
 
 		btnEnter = new JButton ("     Enter!     ");
-		btnEnter.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+		btnEnter.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
 		btnEnter.addActionListener( new ButtonListener());
 
 		binary = new BinaryConverter();
-		txtInsert = new JTextField(8);
-		lblString = new JLabel();
+		txtBinary = new JTextField(50);
+		txtBinary.setEditable(false);
+		txtOctal = new JTextField(50);
+		txtOctal.setEditable(false);
+		txtDecimal = new JTextField(50);
+		txtDecimal.setEditable(false);
+		txtHex = new JTextField(50);
+		txtHex.setEditable(false);
+		txtInsert = new JTextField(15);
+		lblBinary = new JLabel("  Binary Conversion:");
+		lblBinary.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+		lblOctal = new JLabel("   Octal Conversion:");
+		lblOctal.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+		lblDecimal = new JLabel("Decimal Conversion:");
+		lblDecimal.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+		lblHex = new JLabel("    Hex Conversion:");
+		lblHex.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+		lblTitle = new JLabel("                    Conversions                    ");
+		lblTitle.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+		lblBox1 = new JLabel("Select Binary Type:");
+		lblBox1.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
 		listener = new ButtonListener();
 
-		cp.add(lblString);
+		Box box1 = Box.createVerticalBox();
+		grpButtons1 = new ButtonGroup();
+		rdoBinary1 = new JRadioButton("Numerical");
+		rdoBinary2 = new JRadioButton("ASCII");
+
+		/*Box box2 = Box.createVerticalBox();
+		grpButtons2 = new ButtonGroup();
+		rdoEdit = new JRadioButton("Numerical");
+		rdoEdit = new JRadioButton("ASCII");*/
+
+		cp.add(lblTitle);
+		cp.add(lblBinary);
+		cp.add(txtBinary);
+		cp.add(lblOctal);
+		cp.add(txtOctal);
+		cp.add(lblDecimal);
+		cp.add(txtDecimal);
+		cp.add(lblHex);
+		cp.add(txtHex);
+		cp.add(box1);
 		cp.add(txtInsert);
 		cp.add(btnEnter);
 
 		btnEnter.addActionListener(listener);
+
+		box1.add(lblBox1);
+		box1.add(rdoBinary1);
+		box1.add(rdoBinary2);
+
+		grpButtons1.add(rdoBinary1);
+		grpButtons1.add(rdoBinary2);
 
 		setSize(APPLET_WIDTH, APPLET_HEIGHT);
 		setVisible(true);
@@ -53,7 +99,7 @@ public class CalculatorGUI extends JFrame
 			if(source == btnEnter)
 			{
 				String phrase = txtInsert.getText();
-				lblString.setText(binary.getValue(phrase));
+				txtBinary.setText(binary.getValue(phrase));
 			}
 		}
 	}
