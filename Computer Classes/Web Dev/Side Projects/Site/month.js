b1bbb951926befe9ebe721ded95bf21196fda061
Zerocrits <12242864@wayneschools.net>
2016-10-25 08:08:16 -0400

    wer

---
+++ Computer Classes/Web Dev/Side Projects/Site/month.js
@@ -0,0 +1,21 @@
+function getMonth(){
+	var currentTime = new Date()
+	var month = currentTime.getMonth()
+	var year = currentTime.getFullYear()
+		switch(month){
+			case 0: month = "January"; break;
+			case 1: month = "February"; break;
+			case 2: month = "March"; break;
+			case 3: month = "April"; break;
+			case 4: month = "May"; break;
+			case 5: month = "June"; break;
+			case 6: month = "July"; break;
+			case 7: month = "August"; break;
+			case 8: month = "September"; break;
+			case 9: month = "October"; break;
+			case 10: month = "November"; break;
+			case 11: month = "December"; break;
+		}
+		var tString = month + "";
+		document.getElementById('date').innerHTML = tString;
+}
