b0112148f8876c36417d5b6c2dbf2e44e341f52c
Zerocrits <12242864@wayneschools.net>
2017-01-03 08:09:18 -0500

    sdf

--- Computer Classes/Java/2016-2017/MP2/Agram/AgramCard.java
+++ Computer Classes/Java/2016-2017/MP2/Agram/AgramCard.java
@@ -30,16 +30,47 @@ public class AgramCard
 	public String getSuit()
 	{
 		String ranks = "CDHS"; //clubs diamonds hearts spades
+		String ranked = "A23456789TJQK";
+		String lowest = "KS";
 		for(int i = 0; i < deck.length; i++)
 		{
-			if(ranks.indexOf(deck[i].substring(1)) == ranks.indexOf(lead.substring(1)))//null
+			if(ranks.indexOf(deck[i].substring(1)) == ranks.indexOf(lead.substring(1)))//same suit
 			{
 				if(getValue(deck[i]) == true)
 				{
 					return deck[i];
 				}
-			}//add so if its not the same card type it can still find it
+			}
 		}
-		return "No Winnable Card";
+		for(int i = 0; i < deck.length; i++)
+		{
+			if(ranks.indexOf(deck[i].substring(1)) == ranks.indexOf(lead.substring(1)))//same suit
+			{
+				if(getValue(deck[i]) == false)
+				{
+					if(ranked.indexOf(lowest.substring(0,1)) > ranked.indexOf(deck[i].substring(0,1)))
+					{
+						lowest = deck[i];
+					}
+					if(i == deck.length-1)
+					{
+						return lowest;
+					}
+				}
+			}
+		}
+		for(int i = 0; i < deck.length; i++)
+		{
+			if(ranked.indexOf(deck[i].substring(0,1)) < ranked.indexOf(lead.substring(0,1)))
+			{
+				if(ranked.indexOf(lowest.substring(0,1)) > ranked.indexOf(deck[i].substring(0,1)))
+					lowest = deck[i];
+				if(i == deck.length-1)
+				{
+					return lowest;
+				}
+			}
+		}
+		return "No Winnable Card(error)";
 	}
 }
--- Computer Classes/Java/2016-2017/MP2/Agram/AgramRunner.java
+++ Computer Classes/Java/2016-2017/MP2/Agram/AgramRunner.java
@@ -4,14 +4,23 @@ public class AgramRunner
 {
 	public static void main (String[] args)
 	{
-		System.out.println("~~~~Agram Card Game~~~~");
-		String deck = "";
+		boolean choice = true;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please Enter Cards: ");
-		deck = sc.nextLine();
-		Agram game = new Agram(deck);
-		System.out.println("Players Lead: " + game.getPlayer());
-		System.out.println("The best card to pick is: " + game.toString());
+		while(choice == true)
+		{
+			System.out.println("~~~~Agram Card Game~~~~");
+			String deck = "";
+			System.out.print("Please Enter Cards: ");
+			deck = sc.nextLine();
+			Agram game = new Agram(deck);
+			System.out.println("Players Lead: " + game.getPlayer());
+			System.out.println("The best card to pick is: " + game.toString());
+			System.out.print("Would you Like to play again: ");
+			if(sc.nextLine() == "yes");
+				choice = true;
+			System.out.println("\n\n");
+		}
+		System.out.println("Thanks for playing Agram!");
 	}
 
 }
